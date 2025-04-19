c1d60005a9499a4bbba3456568a3847cf6c56180
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-19 15:06:21 -0700

    save data to database and also load from database

--- get_data.py
+++ get_data.py
@@ -1,6 +1,19 @@
 from nba_api.stats.endpoints import leaguegamelog
 import pickle
+import sqlite3
 
+def buildDict(headers, row):
+    game = dict(zip(headers, row))
+    game["SEASON_ID"] = int(game["SEASON_ID"])
+    game["TEAM_ID"] = int(game["TEAM_ID"])
+    game["GAME_ID"] = int(game["GAME_ID"])
+    game["MIN"] = int(game["MIN"])
+    game["FG_PCT"] = float(game["FG_PCT"])
+    game["FG3_PCT"] = float(game["FG3_PCT"])
+    game["FT_PCT"] = float(game["FT_PCT"])
+    game["PLUS_MINUS"] = float(game["PLUS_MINUS"])
+    game["VIDEO_AVAILABLE"] = bool(game["VIDEO_AVAILABLE"])    
+    return game
 
 # d = leaguegamelog.LeagueGameLog(season="2024-25", league_id="00").get_dict()
 
@@ -11,10 +24,28 @@ import pickle
 with open("temp.pkl", "rb") as file_handle:
     d = pickle.load(file_handle)
 
-print(d.keys())
 data = d.get('resultSets')[0]
-print(type(data))
-print(len(data))
-print(data.keys())
-print("headers:", data.get('headers'))
-print("rowSet:", data.get('rowSet')[0])
+
+headers = data.get('headers')
+rows = data.get('rowSet')
+
+conn = sqlite3.connect("leaguegamelog.db")
+cursor = conn.cursor()
+
+with open("schema.sql", "r") as f:
+    cursor.executescript(f.read())
+
+print("successfully made table")
+
+for index, row in enumerate(rows):
+    game = buildDict(headers, row)
+    cursor.execute(f"""
+INSERT OR REPLACE INTO games (
+    {', '.join(headers)}
+) VALUES (
+    {', '.join(':' + h for h in headers)}
+)
+""", game)
+
+conn.commit()
+conn.close()
---
+++ read_data.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+with sqlite3.connect("leaguegamelog.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA table_info(games)")
+    columns = cursor.fetchall()
+
+
+    cursor.execute("SELECT COUNT(*) FROM games")
+    num_games = cursor.fetchone()[0]
+
+
+
+    cursor.execute("SELECT * FROM games")
+
+for col in columns:
+    print(col)
+print()
+print(f"there are {num_games} games in the table")
+for row in cursor:
+    print(row)
