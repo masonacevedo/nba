0ac0bb55a6fd62381f179f1693bd6ff8c57eb8c7
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-28 14:30:37 -0700

    not sure we're using this, but whatever

--- build_database.py
+++ build_database.py
@@ -3,12 +3,12 @@ import pickle
 from nba_api.stats.endpoints import playoffpicture
 
 
-seasons = [str(i) + "-" + str(i+1)[2:] for i in range(2024, 2025)]
+seasons = [str(i) + "-" + str(i+1)[2:] for i in range(1946, 2025)]
 
 conn = sqlite3.connect("playoff_picture.db")
-
-# with open("playoff_picture_schema.sql","r") as f:
-    # cursor.executescript(f.read())
+cursor = conn.cursor()
+with open("playoff_picture_schema.sql","r") as f:
+    cursor.executescript(f.read())
 
 for season in seasons:
     file_name = season + "_playoff_standings.pkl"
@@ -18,9 +18,18 @@ for season in seasons:
     data = d.get('resultSets')[0]
     headers = data.get('headers')
     rows = data.get('rowSet')
-    for header, datum in zip(headers, rows[0]):
-        print(header,":",datum)
+    for row in rows:
+        game = {header:item for item, header in zip(row, headers)}
+        cursor.execute(f"""
+            INSERT OR REPLACE INTO playoff_picture (
+                {', '.join(headers)}
+            ) VALUES (
+                {', '.join(':' + h for h in headers)}
+            )""", game)
 
-    breakpoint()
 
-
+# cursor.execute("SELECT * FROM playoff_picture")
+# games = cursor.fetchall()
+# for g in games:
+    # print(g)
+    input()
